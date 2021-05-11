fcc28de0d93f9ef7680589ef3be3723fd046234f
Jay Zuo <zuojianye@live.com>
2021-05-11 16:08:12 +0800

    Fix error with getCatalogItems

--- basic_sample.js
+++ basic_sample.js
@@ -673,6 +673,7 @@ handlers.getCatalogItems = function (args, context) {
     var cat = server.GetCatalogItems(
         CatalogVersion = "Alpha"
     )
-    element["Catalog"] = cat.Catalog;
+    var element = {},
+    element['Catalog'] = cat.Catalog;
     return element;
 }
